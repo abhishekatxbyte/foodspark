a8b9c498cbea733cba9bf33f30178b7714148991
abhishekatxbyte <abhishek.vaghela.xbyte@gmail.com>
2023-10-09 11:55:44 +0530

    hero section and images added

--- src/Components/HomePageComponents/HeroSection/HeroSection.jsx
+++ src/Components/HomePageComponents/HeroSection/HeroSection.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
-
+import HeroTextSection from './components/HeroTextSection/HeroTextSection'
+import HeroCarousel from './components/HeroCarousel/HeroCarousel'
+import style from './Hero.module.css'
 const HeroSection = () => {
     return (
-        <div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
-            <div>HeroSection 1</div>
+        <div className={style.heroSection}>
+            <HeroTextSection />
+            <HeroCarousel />
         </div>
     )
 }
